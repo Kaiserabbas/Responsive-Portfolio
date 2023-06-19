bcbad706f0b30ed7c0c0e3f54af1c22392c05764
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-19 20:25:03 +0500

    Blur background

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 function toggleDropdown() {
   const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+  const bground = document.getElementById('main');
+  bground.classList.add('blurr');
+  const bgroundLogo = document.getElementById('logo');
+  bgroundLogo.classList.add('blurr');
 }
 
 function closeDropdown() {
   const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = 'none';
+  const bground = document.getElementById('main');
+  bground.classList.remove('blurr');
+  const bgroundLogo = document.getElementById('logo');
+  bgroundLogo.classList.remove('blurr');
 }
 /* eslint-enable no-unused-vars */
