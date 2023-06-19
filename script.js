5ba1df35fef249c73b4e2e3a87653ac92c7ecf37
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-19 17:16:24 +0500

    Add linters and check for error

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-const dropdown = document.getElementById('dropdownContent');
 function toggleDropdown() {
+  const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
 
 function closeDropdown() {
+  const dropdown = document.getElementById('dropdownContent');
   dropdown.style.display = 'none';
 }
 /* eslint-enable no-unused-vars */
