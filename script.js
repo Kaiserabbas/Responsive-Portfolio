d3214697fb9fad3f8a2c6127f87a823850506685
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-06-19 13:02:27 +0500

    Add Dropdown Menu Function

---
+++ script.js
@@ -0,0 +1,9 @@
+function dropdownToggle() {
+  let dropmenu = document.getElementById("drop-content");
+  dropmenu.style.dispaly = dropmenu.style.dispaly === "none" ? "block" : "none";
+}   
+
+function dropClose() {
+  let dropmenu = document.getElementById("drop-content");
+  dropmenu.style.dispaly = none;
+}
