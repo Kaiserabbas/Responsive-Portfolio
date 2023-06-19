c175c82b48621bb72e6d0c83800d5be3df1e3a45
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-19 15:44:58 +0500

    add eslint and modify index,html

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function dropdownToggle() {
-  let dropmenu = document.getElementById("drop-content");
-  dropmenu.style.dispaly = dropmenu.style.dispaly === "none" ? "block" : "none";
-}   
-
-function dropClose() {
-  let dropmenu = document.getElementById("drop-content");
-  dropmenu.style.dispaly = none;
+/* eslint-disable no-unused-vars */
+const dropdown = document.getElementById('dropdownContent');
+function toggleDropdown() {
+  dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
+
+function closeDropdown() {
+  dropdown.style.display = 'none';
+}
+/* eslint-enable no-unused-vars */
